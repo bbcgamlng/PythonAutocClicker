10de59e035d38261bc2947b76def817615122561
John Wendland <115189055+bbcgamlng@users.noreply.github.com>
2023-02-20 17:45:43 -0800

    Update AutoCLicker.py

--- AutoCLicker.py
+++ AutoCLicker.py
@@ -1,7 +1,6 @@
 import pyautogui
 from tkinter import *
 import time
-import keyboard
 
 
 class Window(Frame):
@@ -12,6 +11,7 @@ class Window(Frame):
 
         # widget can take all window
         self.pack(fill=BOTH, expand=1)
+
 
         my_font = ("times", 22, 'bold')
         my_font2 = ("times", 9)
@@ -25,48 +25,41 @@ class Window(Frame):
         self.click_entry = Entry()
         self.click_entry.pack(pady=10)
 
+        time_label = Label(text="Enter Time delay(secound):")
+        time_label.pack(pady=10)
+        self.time_entry = Entry()
+        self.time_entry.pack(pady=10)
+
+
         xcredits.place(x=75,y=80)
         nameauto.place(x=-10,y=40)
-        exitButton.place(x=250, y=490)
-        clickButton.place(x=20, y=350)
-        rightclickButton.place(x=20,y=400)
+        exitButton.place(x=20, y=450)
+        clickButton.place(x=20, y=200)
+        rightclickButton.place(x=20,y=250)
 
     # button functions
     def clickExitButton(self):
         exit()
 
     def clickrightButton(self):
-        time.sleep(3)
+        time.sleep(int(self.time_entry.get()))
         xy = 0
         while xy < int(self.click_entry.get()):
             pyautogui.rightClick()
             xy += 1
 
     def clicktestButton(self):
-        time.sleep(3)
+        time.sleep(int(self.time_entry.get()))
         y = 0
         while y < int(self.click_entry.get()):
             pyautogui.click()
             y += 1
 
-    # auto-clicking function
-    def auto_click(self):
-        while keyboard.is_pressed('q'):
-            pyautogui.click()
-
-    # start auto-clicking when Q key is pressed
-    def start_auto_click(self):
-        if keyboard.is_pressed('q'):
-            self.auto_click()
-        else:
-            self.master.after(10, self.start_auto_click)
-
     def run_auto_click(self):
-        self.start_auto_click()
+        pass
 
 root = Tk()
 app = Window(root)
 root.wm_title("AutoClicker")
 root.geometry("320x550")
-app.run_auto_click() # start auto-clicking detection loop
 root.mainloop()
